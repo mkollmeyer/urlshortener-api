f91c3880fa35ebbf98cdcd00fcf7bfe4c78265e3
Max Kollmeyer <max.kollmeyer@gmail.com>
2025-12-03 22:23:40 -0500

    Created initial webserver

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: http.HandlerFunc(handler),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Connection failed", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, world!"))
+}
